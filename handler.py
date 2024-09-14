c529ba70ddc08849a1187611bc398c8074f4bd65
spark-brc <envpsg@gmail.com>
2024-09-14 09:34:00 -0500

    addd handler

--- handler.py
+++ handler.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import streamlit as st
-# read pcp file from SWAT format
+# read pcp file from SWAT formatß
 
 def read_pcp():
     uploaded_file = st.sidebar.file_uploader("PCP file from SWAT")
     fordf = uploaded_file
     if uploaded_file:
         file_content = uploaded_file.readlines()[2].strip().split()
+
+
         st.write(file_content)
         print(fordf)
         # df = pd.read_csv(
